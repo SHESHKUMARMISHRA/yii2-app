42de9099dbcb0c6788aa8545941b3303ed9ee110
SheshMishra <shesh@netcomlearning.com>
2026-01-09 15:47:16 +0530

    added db backup file with config db changes

--- config/db.php
+++ config/db.php
@@ -1,24 +1,27 @@
 <?php
 
-// return [
-//     'class' => 'yii\db\Connection',
-//     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-//     'username' => 'root',
-//     'password' => '',
-//     'charset' => 'utf8',
-
-//     // Schema cache options (for production environment)
-//     //'enableSchemaCache' => true,
-//     //'schemaCacheDuration' => 60,
-//     //'schemaCache' => 'cache',
-// ];
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2db',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'attributes' => [
+        PDO::MYSQL_ATTR_INIT_COMMAND =>
+            "SET NAMES utf8mb4 COLLATE utf8mb4_general_ci"
+    ],
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
 
 
 // Updated configuration for Docker environment
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=yii2db',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
+// return [
+//     'class' => 'yii\db\Connection',
+//     'dsn' => 'mysql:host=db;dbname=yii2db',
+//     'username' => 'root',
+//     'password' => 'root',
+//     'charset' => 'utf8',
+// ];
